06a71617c3e36730651c176fef4f480ac075ebd9
15615014 <cneeson04@qub.ac.uk>
2025-02-24 02:23:30 +0000

    20250223 EOD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title') | RepairMart</title>
-</head>
-<body>
-<header>RepairMart Header</header>
-@yield('content')    
-<footer>RepairMart Footer</footer>
-</body>
-</html>
+@extends('layouts.clean');
+
+@section('childContent')
+    @include('layouts.partials.header');
+    @yield('content')
+    <footer></footer>
+@endsection
